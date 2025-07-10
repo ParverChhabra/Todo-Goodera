0cecc80da9f3b5b7bf85207a10a932883c81c214
Parver Chhabra <pc@Parvers-MacBook-Air.local>
2025-07-10 17:01:16 +0530

    Update CORS settings for S3 frontend and environment variables

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import { METHODS } from 'http';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: 'http://todoparver.s3-website-us-east-1.amazonaws.com',
-    methods:"GET,POST,PATCH,PUT,DELETE"});
+  app.enableCors({
+    origin: 'http://todoparver.s3-website-us-east-1.amazonaws.com',
+    methods: "GET,POST,PATCH,PUT,DELETE"
+  });
   app.setGlobalPrefix('api');
-
-  await app.listen(3000,"0.0.0.0");
+  await app.listen(3000, "0.0.0.0");
 }
 bootstrap();
+
